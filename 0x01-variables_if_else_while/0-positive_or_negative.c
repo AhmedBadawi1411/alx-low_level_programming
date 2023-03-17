c333beef0f7e03878818a34ba0ae9f6d30b26a2d
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-17 11:18:23 -0700

    pos or nig fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* checker to check if n is pos or neg */
-	if(n = 0){
+	if(n == 0){
 		printf("%d is zero\n" , n);
 	}
 	else if (n > 0){
