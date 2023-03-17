acac8c7110255bc379825ff499736d2891bcff24
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-17 12:00:28 -0700

    alphbey with out q,n

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main : Entry point
+ * @void : Null value
+ *
+ * Description: write the alphbet in lower case excepect q , n
+ * Return: zero
+ */
+
+int main (void)
+{
+	char c;
+	for(c = 'a' ; c >= 'z' ; c++)
+	{
+		if(c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	putchar('\n');
+	}
+	return (0);
+}
