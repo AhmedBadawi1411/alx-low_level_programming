1aad127562993f950045dce597b92444bafb0288
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-17 12:23:16 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void : Null value
+ *
+ * Description: Print alphabet in reverse
+ * Return: Zero value
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'z'; z >= 'a' ; c--)
+		putchr(c);
+	putchar('\n');
+	return (0);
+}
