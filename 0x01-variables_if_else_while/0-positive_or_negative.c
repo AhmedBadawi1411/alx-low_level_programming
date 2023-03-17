ff6b5c90d474c2217a7b898dea792eef23a8c107
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-17 11:27:08 -0700

    l-fix pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,31 @@
 #include <time.h>
 #include <stdio.h>
 
-/* simple function to check number is pos or neg
+/**
+ * main - Check if a number.
+ * @void: Null value
  *
- *
+ * Description: Print if a number is positive, negative or zero
+ * Return: Zero value
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* checker to check if n is pos or neg */
-	if(n == 0)
-		printf("%d is zero\n" , n);
-	else if (n > 0)
-		printf("%d is positive\n" , n);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	else
-		printf("%d is negative\n" , n);
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
