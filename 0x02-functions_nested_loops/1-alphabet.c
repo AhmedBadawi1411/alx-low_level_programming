b4892120d310a40a80347cdff9d1ddee9099d55e
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-21 07:41:12 -0700

    alphabet fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,11 @@
  */
 void print_alphabet(void)
 {
-	int i;
-        for (i = 97; i <= 122; i++)
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar(i);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
