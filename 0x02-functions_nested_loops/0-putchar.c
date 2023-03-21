32789ccb2adf011bc4527e86a12f18d7269e2aa6
ahmed badawi <ahmedbadawi.1411@gmail.com>
2023-03-21 06:38:59 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: Entry Point
+ * @void: Null value
+ *
+ * Description: Function to print _[utchar followed by new line
+ * Return: Zero value
+int main(void)
+{
+	
+	printf("_putchar\n");
+
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+*functions & nested loop* this is the readme for 0x02 low level programming
